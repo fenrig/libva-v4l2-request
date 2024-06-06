30da3268f055f66e96987d7b906284a4303fe3e3
Matthias Van Gestel <maregt0@gmail.com>
2024-06-06 00:45:51 +0000

    fix

--- src/context.c
+++ src/context.c
@@ -122,7 +122,6 @@ VAStatus RequestCreateContext(VADriverContextP context, VAConfigID config_id,
 		rc = v4l2_query_buffer(driver_data->video_fd, output_type,
 				       index, &length, &offset, 1);
 		if (rc < 0) {
-			request_log("v4l2_query_buffer failed: %d (%s)", errno, strerror(errno));
 			status = VA_STATUS_ERROR_ALLOCATION_FAILED;
 			goto error;
 		}
