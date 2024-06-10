d70e9e1942c1bfeca96fc85315f34f8b5a4097d9
Matthias Van Gestel <maregt0@gmail.com>
2024-06-10 19:04:22 +0000

    fix

--- src/surface.c
+++ src/surface.c
@@ -64,6 +64,7 @@ VAStatus RequestCreateSurfaces2(VADriverContextP context, unsigned int format,
 	unsigned int format_width, format_height;
 	unsigned int capture_type;
 	unsigned int index_base;
+	unsigned int pixelformat = V4L2_PIX_FMT_H264_SLICE;
 	unsigned int index;
 	unsigned int i, j;
 	VASurfaceID id;
